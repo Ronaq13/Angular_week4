f5687f5d24921d411d79e9399cecad74c3e3bbea
Ronaq13 <raounaqsharma@gmail.com>
2017-01-10 14:29:39 +0530

    [MAJOR] exercise using $http done

--- conFusion_week3/app/scripts/controllers.js
+++ conFusion_week3/app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
     $scope.filtText = '';
     $scope.showDetails = false;
 
-    $scope.dishes = menuFactory.getDishes();
+    $scope.dishes = {};
+
+    menuFactory.getDishes().then(
+        function(response) {
+            $scope.dishes = response.data;
+        }
+    );
 
 
     $scope.select = function(setTab) {
@@ -66,9 +72,12 @@ angular.module('confusionApp')
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-    var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-
-    $scope.dish = dish;
+    $scope.dish = {};
+    menuFactory.getDish(parseInt($stateParams.id, 10)).then(
+        function(response) {
+            $scope.dish = response.data;
+        }
+    );
 
 }])
 
@@ -91,7 +100,12 @@ angular.module('confusionApp')
 
 // implement the IndexController and About Controller here
 .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
-        $scope.dish = menuFactory.getDish(0);
+        $scope.dish = {};
+        menuFactory.getDish(0).then(
+            function(response) {
+                $scope.dish = response.data;
+            }
+        );
         $scope.promo = menuFactory.getPromotion(0);
         $scope.leader = corporateFactory.getLeader(3);
     }])
