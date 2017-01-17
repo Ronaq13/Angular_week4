0fb19a08f72821f3ee705b7ee07d10c294c53e67
Ronaq13 <raounaqsharma@gmail.com>
2017-01-17 13:28:20 +0530

    [MAJOR] Error handling exercise complete.

--- conFusion_week3/app/scripts/controllers.js
+++ conFusion_week3/app/scripts/controllers.js
@@ -7,10 +7,17 @@ angular.module('confusionApp')
     $scope.tab = 1;
     $scope.filtText = '';
     $scope.showDetails = false;
-    $scope.showMenu = true;
+    $scope.showMenu = false;
     $scope.message = "Loading...";
 
-    $scope.dishes = menuFactory.getDishes().query();
+    menuFactory.getDishes().query(
+        function(response) {
+            $scope.dishes = response;
+            $scope.showMenu = true;
+        },
+        function(response) {
+            $scope.message = "Error:" + response.status + " " + response.statusText;
+        });
 
     $scope.select = function(setTab) {
         $scope.tab = setTab;
@@ -68,35 +75,51 @@ angular.module('confusionApp')
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
     $scope.dish = {};
-    $scope.showDish = true;
-    $scope.message = "Loading...";
-    $scope.dish = menuFactory.getDishes().get({ id: parseInt($stateParams.id, 10) });
+    $scope.showDish = false;
+    $scope.message = "Loading ...";
+    $scope.dish = menuFactory.getDishes().get({ id: parseInt($stateParams.id, 10) })
+        .$promise.then(
+            function(response) {
+                $scope.dish = response;
+                $scope.showDish = true;
+            },
+            function(response) {
+                $scope.message = "Error: " + response.status + " " + response.statusText;
+            }
+        );
 
 }])
 
-.controller('DishCommentController', ['$scope', function($scope) {
+.controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
     $scope.mycomment = { rating: 5, comment: "", author: "", date: "" };
 
     $scope.submitComment = function() {
-
         $scope.mycomment.date = new Date().toISOString();
         console.log($scope.mycomment);
-
         $scope.dish.comments.push($scope.mycomment);
 
+        menuFactory.getDishes().update({ id: $scope.dish.id }, $scope.dish);
         $scope.commentForm.$setPristine();
-
         $scope.mycomment = { rating: 5, comment: "", author: "", date: "" };
-    };
+    }
 }])
 
 // implement the IndexController and About Controller here
 .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
 
-        $scope.showDish = true;
-        $scope.message = "Loading...";
-        $scope.dish = menuFactory.getDishes().get({ id: 0 });
+        $scope.showDish = false;
+        $scope.message = "Loading ...";
+        $scope.dish = menuFactory.getDishes().get({ id: 0 })
+            .$promise.then(
+                function(response) {
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: " + response.status + " " + response.statusText;
+                }
+            );
         $scope.promo = menuFactory.getPromotion(0);
         $scope.leader = corporateFactory.getLeader(3);
     }])
